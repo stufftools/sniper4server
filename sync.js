8723d9f11974eaa362679a85dd618b52a5088ca9
stufftools <54242268+stufftools@users.noreply.github.com>
2019-08-18 17:00:46 +0200

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,1 +1,7 @@
 alert("it works");
+var $updateData = $('<pre>').css('white-space', 'pre-wrap');
+$('.info').append($updateData);
+Asura.onStatusUpdate(
+	function(data){
+		$updateData.text(JSON.stringify(data, null, '\t'));
+	}
