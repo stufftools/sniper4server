5c59c63b5878f6feac6bc318b153c7fdb27ecb98
stufftools <54242268+stufftools@users.noreply.github.com>
2019-08-18 22:56:49 +0200

    Update sniper4sync.js

--- sniper4sync.js
+++ sniper4sync.js
@@ -4,24 +4,23 @@ function init(apikey) {
 	
 	}
 
-
 var $updateData = $('<pre>').css('white-space', 'pre-wrap');
 
 $('.info').append($updateData);
 Asura.onStatusUpdate(
 	function(data){	
-		$updateData.text(JSON.stringify(data, null, '\t'));
+		//$updateData.text(JSON.stringify(data, null, '\t'));
 console.log(data);
 	}
 );
 console.log(userapikey);
 // Execute the console command Game.Info every 5 seconds:
-setInterval(
-	function(){
-		Console.SendCommand("Game.Info");
-	},
-	5000
-);
+//setInterval(
+//	function(){
+//		Console.SendCommand("Game.Info");
+//	},
+//	5000
+//);
 
 // When the response to Game.Info is received, add an info message telling us how many players are connected:
 Console.onMessage(
