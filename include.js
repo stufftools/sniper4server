ace24482e2fdf19dc9f34b61cb5ee599fc3303d6
stufftools <54242268+stufftools@users.noreply.github.com>
2019-08-25 21:40:27 +0200

    Update include.js

--- include.js
+++ include.js
@@ -1,5 +1,5 @@
 var apikey = "apikeylongkey";
-$.getScript( "https://stufftools.github.io/sniper4server/sync.js", function( data, textStatus, jqxhr ) {
+$.getScript( "https://stufftools.github.io/sniper4server/sniper4sync.js", function( data, textStatus, jqxhr ) {
 init(apikey);
   console.log( data ); // Data returned
   console.log( textStatus ); // Success
